2bf637e9271b2caf7f9c594710d85b2d04c307f2
Fede Gilles <fedegilles@gmail.com>
2026-02-17 17:15:53 -0300

    update footer link

--- app/footer.tsx
+++ app/footer.tsx
@@ -70,7 +70,7 @@ export function Footer() {
   return (
     <footer className="mt-24 border-t border-zinc-100 px-0 py-4 dark:border-zinc-800">
       <div className="flex items-center justify-between">
-        <a href="https://github.com/ibelick/nim" target="_blank">
+        <a href="https://github.com/fedegilles" target="_blank">
           <TextLoop className="text-xs text-zinc-500">
             <span>© 2025 Fedegilles.</span>
             <span>Pizza box theory</span>
